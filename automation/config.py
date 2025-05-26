bac547791f2621c0222deb6bc15affbf66271f4f
Andre Lubambi <andrelubambi36@gmail.com>
2025-05-26 16:32:32 +0100

    Starting chalenge

---
+++ automation/config.py
@@ -0,0 +1,4 @@
+URL_LOGIN = "https://exemplo.com/login"
+USUARIO = "meuUsuario"
+SENHA = "minhaSenha123"
+CAMINHO_DRIVER = "chromedriver.exe"
---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.cummon.by import By
+
+def iniciar_driver():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    return driver
+
+def main():
+    driver = iniciar_driver()
+    driver.get("https://www.google.com")
+    print(driver.title)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
